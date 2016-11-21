3099f279b6b1a8c0e8f263262c79a50fd5e474ab
damiendevienne <damien.de-vienne@univ-lyon1.fr>
2016-11-21 14:21:26 +0100

    added licence

--- PIPELINE/CreateIndex.py
+++ PIPELINE/CreateIndex.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python
 
+#    This file is part of Lifemap.
+
+#    Lifemap is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Lifemap is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Lifemap.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import psycopg2 ##for postgresql connection
 
 ##CONNECT TO POSTGRESQL/POSTGIS DATABASE

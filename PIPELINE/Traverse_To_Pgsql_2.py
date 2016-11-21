a1f217380b3403209349e42c557632ae6e166acb
damiendevienne <damien.de-vienne@univ-lyon1.fr>
2016-11-21 14:22:25 +0100

    added licence

--- PIPELINE/Traverse_To_Pgsql_2.py
+++ PIPELINE/Traverse_To_Pgsql_2.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python
 
+#    This file is part of Lifemap.
+
+#    Lifemap is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Lifemap is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Lifemap.  If not, see <http://www.gnu.org/licenses/>.
+
+
 # FEV, 5 2016
 # WE REMOVE THE WRITING TO JSON AND trees. This is performed by another code.
 # This code is more complete than the previous one.

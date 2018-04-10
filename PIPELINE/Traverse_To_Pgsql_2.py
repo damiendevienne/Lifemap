0f31129789fdcfb4701be795581ccc748e50fed3
de Vienne <damien.de-vienne@univ-lyon1.fr>
2018-04-11 00:22:55 +0200

    new getTree function

--- PIPELINE/Traverse_To_Pgsql_2.py
+++ PIPELINE/Traverse_To_Pgsql_2.py
@@ -75,7 +75,8 @@ if (groupnb=="3"):
 
 t.zoomview = np.ceil(np.log2(30/t.ray));
 
-print "t.x: --------------------      " + t.x
+print "t.x: --------------------      " 
+print t.x
 
 #specis and node ids
 nbsp = len(t)

c552fed0460480783fe1484c3c8612186411cc71
de Vienne <damien.de-vienne@univ-lyon1.fr>
2018-04-25 15:56:00 +0200

    added wait messages

--- PIPELINE/CreateIndex.py
+++ PIPELINE/CreateIndex.py
@@ -8,19 +8,24 @@ try:
 except:
     print "I am unable to connect to the database"
 
+print 'Creating index...'
 cur = conn.cursor()
 cur.execute('CREATE INDEX linesid ON lines USING GIST(way);')
 cur.execute('CREATE INDEX pointsid ON points USING GIST(way);')
 cur.execute('CREATE INDEX polygid ON polygons USING GIST(way);')
 conn.commit()
 
+print 'Clustering...'
 cur.execute('CLUSTER lines USING linesid;')
 cur.execute('CLUSTER points USING pointsid;')
 cur.execute('CLUSTER polygons USING polygid;')
 conn.commit()
 
+print 'Analyzing...'
 cur.execute('ANALYZE lines;')
 cur.execute('ANALYZE points;')
 cur.execute('ANALYZE polygons;')
 conn.commit()
+
+print 'DONE...'
 

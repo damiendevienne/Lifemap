c6a393266f4a08ae19036dbc8e751b49452bd052
Damien de Vienne <damien.de-vienne@univ-lyon1.fr>
2019-09-13 16:28:12 +0200

    new code

--- PIPELINE/StoreWholeNcbiInSolr.py
+++ PIPELINE/StoreWholeNcbiInSolr.py
@@ -3,7 +3,6 @@
 ## This code allows storing the whole NCBI tree in solr. Useful we think.
 import sys,os
 from ete3 import Tree
-import numpy as np
 from getTrees_fun import getTheTrees
 
 T = getTheTrees()
@@ -11,20 +10,17 @@ t = T['1'].detach()
 t.rank = 'no rank'
 ##traverse first time:
 for n in t.traverse():
-    n.path = [];
+    n.path_taxid = [n.taxid];
+    n.path_sci_name = [n.sci_name]
+    n.path_rank = [n.rank]
     node = n
     while node.up:
-        n.path.append(node.up.taxid)
+        n.path_taxid.append(node.up.taxid)
+        n.path_sci_name.append(node.up.sci_name)
+        n.path_rank.append(node.up.rank)
         node = node.up
-    ##reformat the texts
-    n.common_name_all = ', '.join(n.common_name)
-    n.common_name = n.common_name[0] if len(n.common_name)>0 else ""
-    ##we create a 'long' common name. the common name going to db is only the first of the list 
-    n.common_name = n.common_name.replace("'","''");
-    n.rank = n.rank.replace("'","''");
-    n.sci_name = n.sci_name.replace("'","''")
-    n.authority = n.authority.replace("'","''")
-    n.synonym = n.synonym.replace("'","''")
+    # n.rank = n.rank.replace("'","''");
+    # n.sci_name = n.sci_name.replace("'","''")
  
 
 ##traverse to write
@@ -34,16 +30,20 @@ addi.write('[\n')
 for n in t.traverse():
     addi.write("\t{\n");
     addi.write("\t\t\"taxid\":\"%s\",\n" % n.taxid);
-    addi.write("\t\t\"ascend\":[");
-    for k in n.path[:-1]:
+    addi.write("\t\t\"sci_name\":\"%s\",\n" % n.sci_name);
+    addi.write("\t\t\"rank\":\"%s\",\n" % n.rank);
+    addi.write("\t\t\"ascend_taxids\":[");
+    for k in n.path_taxid[:-1]:
         addi.write("%s," % k)
     addi.write("1],\n")   
-    addi.write("\t\t\"sci_name\":\"%s\",\n" % n.sci_name);
-    addi.write("\t\t\"authority\":\"%s\",\n" % n.authority);
-    addi.write("\t\t\"synonym\":\"%s\",\n" % n.synonym);
-    addi.write("\t\t\"common_name\":\"%s\",\n" % n.common_name);
-    addi.write("\t\t\"common_name_all\":\"%s\",\n" % n.common_name_all);
-    addi.write("\t\t\"rank\":\"%s\"\n\t},\n" % n.rank);
+    addi.write("\t\t\"ascend_sci_names\":[");
+    for k in n.path_sci_name[:-1]:
+        addi.write("%s," % k)
+    addi.write("root],\n")   
+    addi.write("\t\t\"ascend_ranks\":[");
+    for k in n.path_rank[:-1]:
+        addi.write("%s," % k)
+    addi.write("no rank]\n\t},\n")   
     
 ##remove unwanted last character(,) of json file
 addi.close()
